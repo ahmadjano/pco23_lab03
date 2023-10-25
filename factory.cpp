0d965a05995cb8c0bf79fe0d69505d941228dfa1
AdrianRogner <100291212+AdrianRogner@users.noreply.github.com>
2023-10-25 17:20:24 +0200

    Arrêt des threads implémentés

---
+++ factory.cpp
@@ -0,0 +1,169 @@
+#include "factory.h"
+#include "extractor.h"
+#include "costs.h"
+#include "wholesale.h"
+#include <pcosynchro/pcothread.h>
+#include <iostream>
+
+WindowInterface* Factory::interface = nullptr;
+
+
+Factory::Factory(int uniqueId, int fund, ItemType builtItem, std::vector<ItemType> resourcesNeeded)
+    : Seller(fund, uniqueId), resourcesNeeded(resourcesNeeded), itemBuilt(builtItem), nbBuild(0)
+{
+    assert(builtItem == ItemType::Chip ||
+           builtItem == ItemType::Plastic ||
+           builtItem == ItemType::Robot);
+
+    interface->updateFund(uniqueId, fund);
+    interface->consoleAppendText(uniqueId, "Factory created");
+}
+
+void Factory::setWholesalers(std::vector<Wholesale *> wholesalers) {
+    Factory::wholesalers = wholesalers;
+
+    for(Seller* seller: wholesalers){
+        interface->setLink(uniqueId, seller->getUniqueId());
+    }
+}
+
+ItemType Factory::getItemBuilt() {
+    return itemBuilt;
+}
+
+int Factory::getMaterialCost() {
+    return getCostPerUnit(itemBuilt);
+}
+
+bool Factory::verifyResources() {
+    for (auto item : resourcesNeeded) {
+        if (stocks[item] == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void Factory::buildItem() {
+
+    // TODO
+    //Vérifier qu'on puisse payer l'employé pour assembler l'objet
+    int employeCost = getEmployeeSalary(getEmployeeThatProduces(getItemBuilt()));
+    if (money < employeCost) {
+        /* Pas assez d'argent*/
+        /* Attend des jours meilleurs */
+        return;
+    }
+
+    //Payer l'employé
+    money -= employeCost;
+
+    //Temps simulant l'assemblage d'un objet.
+    PcoThread::usleep((rand() % 100) * 100000);
+
+    // TODO´
+    //Diminuer de 1 les ressources utilisées pour construire
+    for (ItemType item : resourcesNeeded) {
+        stocks[item]--;
+    }
+
+    //Construction d'un objet
+    //Statistique
+    nbBuild++;
+
+    //Incrémentation du stock
+    stocks[itemBuilt]++;
+
+    interface->consoleAppendText(uniqueId, "Factory have build a new object");
+}
+
+void Factory::orderResources() {
+
+    // TODO - Itérer sur les resourcesNeeded et les wholesalers disponibles
+    for(ItemType it : resourcesNeeded){
+        for(Wholesale* w : wholesalers){
+
+            int price = getCostPerUnit(it);
+
+            if(price > money){
+                interface->consoleAppendText(uniqueId, QString("Not enough money"));
+                return;
+            }
+
+            int facture = w->trade(it, 1);
+
+            if (facture > 0) {
+                money -= facture; // `facture` should be equal to `price` here.
+                stocks[it]++;
+
+                interface->consoleAppendText(uniqueId, QString("Bought %1 ").arg(1) %
+                                             getItemName(it) % QString(" for %1").arg(price));
+            } else {
+                interface->consoleAppendText(uniqueId, QString("Seller has shortage in stock."));
+            }
+
+        }
+    }
+
+    //Temps de pause pour éviter trop de demande
+    PcoThread::usleep(10 * 100000);
+
+}
+
+void Factory::run() {
+    if (wholesalers.empty()) {
+        std::cerr << "You have to give to factories wholesalers to sales their resources" << std::endl;
+        return;
+    }
+    interface->consoleAppendText(uniqueId, "[START] Factory routine");
+
+    while (true /* TODO terminaison*/) {
+        if(PcoThread::thisThread()->stopRequested()){
+            return;
+        }
+
+        if (verifyResources()) {
+            buildItem();
+        } else {
+            orderResources();
+        }
+        interface->updateFund(uniqueId, money);
+        interface->updateStock(uniqueId, &stocks);
+    }
+    interface->consoleAppendText(uniqueId, "[STOP] Factory routine");
+}
+
+std::map<ItemType, int> Factory::getItemsForSale() {
+    return std::map<ItemType, int>({{itemBuilt, stocks[itemBuilt]}});
+}
+
+int Factory::trade(ItemType it, int qty) {
+    // TODO
+
+    if(stocks.at(it) < qty){
+        return 0;
+    }
+    int cost = getMaterialCost() * qty;
+    stocks.at(it) -= qty;
+    money += cost;
+
+    return cost;
+}
+
+int Factory::getAmountPaidToWorkers() {
+    return Factory::nbBuild * getEmployeeSalary(getEmployeeThatProduces(itemBuilt));
+}
+
+void Factory::setInterface(WindowInterface *windowInterface) {
+    interface = windowInterface;
+}
+
+PlasticFactory::PlasticFactory(int uniqueId, int fund) :
+    Factory::Factory(uniqueId, fund, ItemType::Plastic, {ItemType::Petrol}) {}
+
+ChipFactory::ChipFactory(int uniqueId, int fund) :
+    Factory::Factory(uniqueId, fund, ItemType::Chip, {ItemType::Sand, ItemType::Copper}) {}
+
+RobotFactory::RobotFactory(int uniqueId, int fund) :
+    Factory::Factory(uniqueId, fund, ItemType::Robot, {ItemType::Chip, ItemType::Plastic}) {}
---
+++ wholesale.cpp
@@ -0,0 +1,104 @@
+#include "wholesale.h"
+#include "factory.h"
+#include "costs.h"
+#include <iostream>
+#include <pcosynchro/pcothread.h>
+
+WindowInterface* Wholesale::interface = nullptr;
+
+Wholesale::Wholesale(int uniqueId, int fund)
+    : Seller(fund, uniqueId)
+{
+    interface->updateFund(uniqueId, fund);
+    interface->consoleAppendText(uniqueId, "Wholesaler Created");
+
+}
+
+void Wholesale::setSellers(std::vector<Seller*> sellers) {
+    this->sellers = sellers;
+
+    for(Seller* seller: sellers){
+        interface->setLink(uniqueId, seller->getUniqueId());
+    }
+}
+
+void Wholesale::buyResources() {
+    auto s = Seller::chooseRandomSeller(sellers);
+    auto m = s->getItemsForSale();
+    auto i = Seller::chooseRandomItem(m);
+
+    if (i == ItemType::Nothing) {
+        /* Nothing to buy... */
+        return;
+    }
+
+    int qty = rand() % 5 + 1;
+    int price = qty * getCostPerUnit(i);
+
+    interface->consoleAppendText(uniqueId, QString("I would like to buy %1 of ").arg(qty) %
+                                 getItemName(i) % QString(" which would cost me %1").arg(price));
+
+    // Ensure that we have enough money to buy what we want.
+    if (price > money) {
+        interface->consoleAppendText(uniqueId, QString("Not enough money"));
+        return;
+    }
+
+    // Request the trade to Extractor/Factory.
+    int facture = s->trade(i, qty);
+
+    // If the seller accepted the purchase.
+    if (facture > 0) {
+        money -= facture; // `facture` should be equal to `price` here.
+        stocks.at(i) += qty;
+
+        interface->consoleAppendText(uniqueId, QString("Bought %1 ").arg(qty) %
+                                     getItemName(i) % QString(" for %1").arg(price));
+    } else {
+        interface->consoleAppendText(uniqueId, QString("Seller has shortage in stock."));
+    }
+
+}
+
+void Wholesale::run() {
+
+    if (sellers.empty()) {
+        std::cerr << "You have to give factories and mines to a wholeseler before launching is routine" << std::endl;
+        return;
+    }
+
+    interface->consoleAppendText(uniqueId, "[START] Wholesaler routine");
+    while (true /* TODO terminaison*/) {
+        if(PcoThread::thisThread()->stopRequested()){
+            return;
+        }
+        buyResources();
+        interface->updateFund(uniqueId, money);
+        interface->updateStock(uniqueId, &stocks);
+        //Temps de pause pour espacer les demandes de ressources
+        PcoThread::usleep((rand() % 10 + 1) * 100000);
+    }
+    interface->consoleAppendText(uniqueId, "[STOP] Wholesaler routine");
+}
+
+std::map<ItemType, int> Wholesale::getItemsForSale() {
+    return stocks;
+}
+
+int Wholesale::trade(ItemType it, int qty) {
+    if (stocks[it] < qty) {
+        return 0;
+    }
+
+    // Accept the purchase otherwise.
+    int cost = getCostPerUnit(it) * qty;
+
+    getItemsForSale().at(it) -= qty; // Update the stock.
+    money += cost;
+
+    return cost;
+}
+
+void Wholesale::setInterface(WindowInterface *windowInterface) {
+    interface = windowInterface;
+}
